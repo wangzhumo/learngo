66a0462dea6529e2e954674fc3a59d814e8937cc
wangzhumo <wangzhumoo@gmail.com>
2021-06-21 21:35:56 +0800

    [feature]channel -1

--- goroutine/channel/channel.go
+++ goroutine/channel/channel.go
@@ -1,11 +1,34 @@
 package channel
 
+import (
+	"fmt"
+	"time"
+)
+
+func workerTask(taskId int,c chan int) {
+	for  {
+		fmt.Printf("goroutine id = %d value = %d \n",taskId, <-c)
+	}
+}
+
 func channelDemo()  {
-	c := make(chan int)
-	c<-1
+	var channels [10]chan int
+	for i := 0; i < 10; i++ {
+		channels[i] = make(chan int)
+		go workerTask(i,channels[i])
+	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- 3*i
+	}
+
+	for i := 0; i < 10; i++ {
+		channels[i] <- 8*i
+	}
 }
 
 
 func RunChannel()  {
 	channelDemo()
+	time.Sleep(time.Second)
 }
