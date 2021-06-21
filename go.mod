05948efe90dbc850e55c3b65bee6d0026e1a204c
wangzhumo <wangzhumoo@gmail.com>
2021-06-22 01:38:49 +0800

    [feature]修改channel 发送

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module wangzhumo.com/learngo
 go 1.16
 
 require (
+	github.com/google/renameio v0.1.0 // indirect
+	github.com/kisielk/gotool v1.0.0 // indirect
 	github.com/yuin/goldmark v1.3.8 // indirect
 	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 	golang.org/x/tools v0.1.3 // indirect
+	golang.org/x/tools/gopls v0.7.0 // indirect
 )
--- goroutine/channel/channel.go
+++ goroutine/channel/channel.go
@@ -5,30 +5,52 @@ import (
 	"time"
 )
 
-func workerTask(taskId int,c chan int) {
-	for  {
-		fmt.Printf("goroutine id = %d value = %d \n",taskId, <-c)
+// 创建Worker 并且需要返回 chan
+func createWorker(taskId int) chan<- int {
+	c := make(chan int)
+	go workerTask(taskId, c)
+	return c
+}
+
+func workerTask(taskId int, c chan int) {
+	for value := range c {
+		fmt.Printf("goroutine id = %d value = %d \n",
+			taskId, value)
 	}
 }
 
-func channelDemo()  {
-	var channels [10]chan int
+func channelDemo() {
+	var channels [10]chan<- int
 	for i := 0; i < 10; i++ {
-		channels[i] = make(chan int)
-		go workerTask(i,channels[i])
+		channels[i] = createWorker(i)
 	}
 
 	for i := 0; i < 10; i++ {
-		channels[i] <- 3*i
+		channels[i] <- 3 * i
 	}
 
 	for i := 0; i < 10; i++ {
-		channels[i] <- 8*i
+		channels[i] <- 8 * i
 	}
+
+	time.Sleep(time.Microsecond)
 }
 
+func bufferedChannel() {
+	c := make(chan int, 3)
+	go workerTask(510, c)
 
-func RunChannel()  {
+	c <- 21
+	c <- 22
+	c <- 23
+	c <- 24
+
+	time.Sleep(time.Microsecond)
+}
+
+func RunChannel() {
 	channelDemo()
+	bufferedChannel()
+
 	time.Sleep(time.Second)
 }
