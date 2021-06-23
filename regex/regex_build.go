f4efd9a0b27e4f00f57d1fd90fe8b2d40af1f973
wangzhumo <wangzhumoo@gmail.com>
2021-06-24 00:59:26 +0800

    [feature]regexp

--- regex/regex_build.go
+++ regex/regex_build.go
@@ -5,10 +5,15 @@ import (
 	"regexp"
 )
 
-const httpUrl = "My email is wangzhumoo@gmail.com"
+const httpUrl = "My email is wangzhumoo@gmail.com@dfjak"
 
 func RunRegexTest() {
-	r := regexp.MustCompile("wangzhumoo@gmail.com")
-	match := r.FindString(httpUrl)
-	fmt.Println(match)
+	// .*  0个或者多个
+	// .+  1个或者多个
+	r1 := regexp.MustCompile(".+@.+.\\..+")
+	r2 := regexp.MustCompile(`[a-zA-Z0-9]+@.+.\.[a-zA-Z0-9]+`)
+	match1 := r1.FindString(httpUrl)
+	match2 := r2.FindString(httpUrl)
+	fmt.Println(match1)
+	fmt.Println(match2)
 }
