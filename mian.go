68e9db49c581f516e57390ef2e148c1d06a1242c
wangzhumo <wangzhumoo@gmail.com>
2021-06-30 23:33:15 +0800

    [feature]修改文件换行符

--- mian.go
+++ mian.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-
 	"wangzhumo.com/learngo/crawler"
-	"wangzhumo.com/learngo/regex"
 )
 
 func main() {
 	fmt.Println(">>>>>>>>>>>>>>start golang application")
 	crawler.RunCrawlerTask()
-	regex.RunRegexTest()
+
 	fmt.Println(">>>>>>>>>>>>>>end golang application")
 }
