dd80d765aa1986dbafa7decaaac4b43e851f8949
wangzhumo <wangzhumoo@gmail.com>
2021-07-23 01:15:22 +0800

    [feature]启动App

--- lottery/web/main.go
+++ lottery/web/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"com.wangzhumo.lottery/bootstrap"
+	"com.wangzhumo.lottery/web/middleware/identity"
+	"com.wangzhumo.lottery/web/routes"
+	"fmt"
+)
 
+const Port = 8001
+
+func newApp() *bootstrap.Bootstrapper {
+	// 初始化Application
+	app:= bootstrap.New("Lottery System", "wangzhumo")
+	app.Bootstrap()
+
+	// 中间件
+	app.Configure(identity.Configure)
+	// 自己的Route
+	app.Configure(routes.Configure)
+	return app
+}
+
+
+func main() {
+	app:= newApp()
+	app.Listen(fmt.Sprintf(":%d",Port))
 }
